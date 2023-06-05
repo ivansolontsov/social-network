bd7d5a60b7f051996e194d06cb5224556a03b5f2
Ivan Vasil <ivansolontsov@yandex.ru>
2023-06-05 15:41:24 +0400

    header example block

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/common/Header/Header'
 import '../styles/globals.scss'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
